6235e14eeb8540193fea3512983e7cc846608623
Marvin <mezepedaq@miumg.edu.gt>
2026-02-17 20:49:47 -0600

    se agrego modulo para venta

--- M_tres.php
+++ M_tres.php
@@ -1,65 +1,49 @@
-<?php
-$connectio = mysqli_connect("localhost", "root", "", "ferre1");
+<?php 
 
-$id = "";
-$name = "";
+$conection = mysqli_connect("localhost", "root", "", "ferre2");
+if($_SERVER['REQUEST_METHOD'] == "POST"){
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $fecha = $_POST['date'];
+   $vendedor = $_POST['seller'];
 
-    $id= $_POST['id'];
-    $adres = $_POST['Adress'];
-    $numphone = $_POST['numphone'];
-    $name = $_POST['name'];
+   $sql = "INSERT INTO venta (Fecha, Vendedor) VALUES ('$fecha', '$vendedor')";
+   mysqli_query($conection, $sql);
 
-    $sql = "INSERT INTO cliente (Id_cliente, Direccion, Telefono, Nombre) VALUES('$id', '$adres', '$numphone', '$name')";
+    $NewID = mysqli_insert_id($conection);
+    $NewTable = "SELECT * FROM venta WHERE IdVenta = $NewID";
+    $Newdatos = mysqli_query($conection, $NewTable);
 
-    $dato = mysqli_query($connectio, $sql);
-
-
-    // if($dato){
-    //     echo "inside data" . $dato;
-    // }
-    // else
-    //     {
-    //         echo "exit devil ";
-    //     }
 }
 
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <br>
     <form method="POST">
-        Enter name
-        <input type="text" name="name" required>
+        Fecha
+        <input type="date" name="date" required>
         <br><br>
-        Ingrese code
-        <input type="number" name="id" required>
-        <br><br>
-         Ingrese Adress
-        <input type="text" name="Adress" required>
-        <br><br>
-         Ingrese number phone
-        <input type="text" name="numphone" required>
+        Vendedor
+        <input type="text" name="seller" required>
         <br><br>
         <input type="submit" value="Ingresar">
-    </form>
-    <br>
+        <a href="Principal.php">
+            <button>Salir</button>
 </body>
 </html>
 
-<?php 
-
-if($id != "" && $name != ""){
-    echo "ID: ". $id. "<br>";
-    echo "name: ". $name;
-
-}
-
+<?php
+if(isset($Newdatos) && $Newdatos){
+    while($fila = mysqli_fetch_assoc($Newdatos)){
+        echo $fila ['Fecha']. "<br>";
+        echo $fila ['vndedor']. "<br>";
+    }
+}   
 ?>
